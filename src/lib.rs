f9e96673117f70f21baadeaeecdb13a2ab76b2c9
Developer-Abnam <abdulsamadnuradeen@gmail.com>
2025-06-28 00:49:43 -0900

    Completed TODOs for Week Two Exercises

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,40 @@ use hex::{decode, encode};
 
 pub fn decode_hex(hex_str: &str) -> Result<Vec<u8>, String> {
     // TODO: Decode hex string into Vec<u8>, return error string on failure
+    if hex_str.len() % 2 != 0 {
+        return Err("Hex string must have even length".to_string());
+    }
+    let decoded = decode(hex_str);
+    decoded.map_err(|e| e.to_string())
 }
 
 pub fn to_big_endian(bytes: &[u8]) -> Vec<u8> {
     // TODO: Reverse the byte order of input slice and return as Vec<u8>
+    let mut reversed_vec = bytes.to_vec();
+    reversed_vec.reverse();
+    reversed_vec
 }
 
 pub fn bytes_to_hex(bytes: &[u8]) -> String {
     // TODO: Implement conversion of bytes slice to hex string
+    encode(bytes)
 }
 
 pub fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, hex::FromHexError> {
     // TODO: Implement conversion of hex string to bytes vector
+    decode(hex)
 }
 
 pub fn swap_endian_u32(num: u32) -> [u8; 4] {
     // TODO: Implement little-endian byte swap for u32
+    num.to_le_bytes()
 }
 
 pub fn parse_satoshis(input: &str) -> Result<u64, String> {
     // TODO: Parse input string to u64, return error string if invalid
+    input
+        .parse::<u64>()
+        .map_err(|e| format!("Invalid satoshi amount: {}", e))
 }
 
 pub enum ScriptType {
@@ -32,6 +46,11 @@ pub enum ScriptType {
 
 pub fn classify_script(script: &[u8]) -> ScriptType {
     // TODO: Match script pattern and return corresponding ScriptType
+    match script {
+        [0x76, 0xa9, ..] => ScriptType::P2PKH,
+        [0x00, 0x14, ..] => ScriptType::P2WPKH,
+        _ => ScriptType::Unknown,
+    }
 }
 
 // TODO: complete Outpoint tuple struct
@@ -39,6 +58,7 @@ pub struct Outpoint();
 
 pub fn read_pushdata(script: &[u8]) -> &[u8] {
     // TODO: Return the pushdata portion of the script slice (assumes pushdata starts at index 2)
+    &script[2..22]
 }
 
 pub trait Wallet {
@@ -52,15 +72,18 @@ pub struct TestWallet {
 impl Wallet for TestWallet {
     fn balance(&self) -> u64 {
         // TODO: Return the wallet's confirmed balance
+        self.confirmed
     }
 }
 
 pub fn apply_fee(balance: &mut u64, fee: u64) {
     // TODO: Subtract fee from mutable balance reference
+    *balance -= fee
 }
 
 pub fn move_txid(txid: String) -> String {
     // TODO: Return formatted string including the txid for display or logging
+    format!("txid: {}", txid)
 }
 
 // TODO: Add necessary derive traits
@@ -73,6 +96,11 @@ pub enum Opcode {
 impl Opcode {
     pub fn from_byte(byte: u8) -> Result<Self, String> {
         // TODO: Implement mapping from byte to Opcode variant
+        match byte {
+            0xac => Ok(Opcode::OpChecksig),
+            0x76 => Ok(Opcode::OpDup),
+            _ => Err(format!("Invalid opcode: 0x{:02x}", byte)),
+        }
     }
 }
 
@@ -85,4 +113,5 @@ pub struct UTXO {
 
 pub fn consume_utxo(utxo: UTXO) -> UTXO {
     // TODO: Implement UTXO consumption logic (if any)
+    utxo
 }
